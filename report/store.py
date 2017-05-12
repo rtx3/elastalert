11e5e176d41a8b32fc7844cb38c1922c542387d1
rtx3 <r@rtx3.com>
2017-05-12 17:16:00 +0800

    new py file

---
+++ report/store.py
@@ -0,0 +1,11 @@
+from elastalert.enhancements import BaseEnhancement
+
+class DbStore(BaseEnhancement):
+
+    # The enhancement is run against every match
+    # The match is passed to the process function where it can be modified in any way
+    # ElastAlert will do this for each enhancement linked to a rule
+    def process(self, match):
+        if 'domain' in match:
+            url = "http://who.is/whois/%s" % (match['domain'])
+            match['domain_whois_link'] = url
