d28661bded6e4b59c63ec3ca83fdf68b539783c7
rtx3 <r@rtx3.com>
2017-05-15 13:59:04 +0800

    new store

--- elastalert/store.py
+++ elastalert/store.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from enhancements import BaseEnhancement
 from util import elastalert_logger
+import json
 
 class MyEnhancement(BaseEnhancement):
 
@@ -8,4 +9,4 @@ class MyEnhancement(BaseEnhancement):
     # The match is passed to the process function where it can be modified in any way
     # ElastAlert will do this for each enhancement linked to a rule
     def process(self, match):
-        elastalert_logger.info("MATCH :" % match)
+        elastalert_logger.info("MATCH :" % json.dumps(match))
